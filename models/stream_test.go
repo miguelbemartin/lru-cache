6b4578387541f7d4b57f557cc6869415dd48e92c
Miguel Ángel Martín <miguelbemartin@gmail.com>
2019-07-21 21:27:32 +0200

    Add test for Append function

---
+++ models/stream_test.go
@@ -0,0 +1,60 @@
+package models
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStream_Append(t *testing.T) {
+	var tests = []struct {
+		elements     []Element
+		sizeExpected int
+		limit        int
+	}{
+		{
+			elements: []Element{
+				{
+					Key:   "key1",
+					Value: "value 1",
+				},
+				{
+					Key:   "key2",
+					Value: "value 2",
+				},
+			},
+			sizeExpected: 2,
+			limit:        10,
+		},
+		{
+			elements: []Element{
+				{
+					Key:   "key1",
+					Value: "value 1",
+				},
+				{
+					Key:   "key2",
+					Value: "value 2",
+				},
+			},
+			sizeExpected: 1,
+			limit:        1,
+		},
+	}
+
+	for i, test := range tests {
+		stream := Stream{
+			Limit:  test.limit,
+			Length: 0,
+		}
+
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			for _, element := range test.elements {
+				stream.Append(&element)
+			}
+			if stream.Length != test.sizeExpected {
+				t.Errorf("Test failed: wrong size: %d\ngot:  %d", test.sizeExpected, stream.Length)
+			}
+		})
+	}
+
+}
