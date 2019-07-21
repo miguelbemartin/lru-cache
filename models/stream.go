62bc9e810af4ef6a8ab9daed85ecbd5b60772519
Miguel Ángel Martín <miguelbemartin@gmail.com>
2019-07-21 22:47:31 +0200

    Add test function Get

--- models/stream.go
+++ models/stream.go
@@ -15,15 +15,23 @@ func (s *Stream) Append(newElement *Element) {
 	if s.Length == 0 {
 		s.start = newElement
 		s.end = newElement
+		s.Length++
+	} else if s.Length == s.Limit && s.Limit == 1 {
+		s.start = newElement
+		s.end = newElement
+	} else if s.Length == s.Limit {
+		//Reset the first element
+		s.start = s.start.next
+
+		//Set the last element
+		lastElement := s.end
+		lastElement.next = newElement
+		s.end = newElement
+		s.end.next = nil
 	} else {
 		lastElement := s.end
 		lastElement.next = newElement
 		s.end = newElement
-	}
-
-	if s.Length == s.Limit {
-		s.start = s.start.next
-	} else {
 		s.Length++
 	}
 }
@@ -37,7 +45,7 @@ func (s *Stream) Get(key string) (*Element, error) {
 
 	for currentElement.Key != key {
 		if currentElement.next == nil {
-			return nil, errors.New("no such Element found")
+			return nil, errors.New("no such element found")
 		}
 		currentElement = currentElement.next
 	}
--- models/stream_test.go
+++ models/stream_test.go
@@ -56,5 +56,49 @@ func TestStream_Append(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestStream_Get(t *testing.T) {
+	var tests = []struct {
+		elements      []Element
+		limit         int
+		key           string
+		errorExpected string
+	}{
+		{
+			elements: []Element{
+				{
+					Key:   "key1",
+					Value: "value 1",
+				},
+				{
+					Key:   "key2",
+					Value: "value 2",
+				},
+			},
+			limit:         1,
+			key:           "key1",
+			errorExpected: "no such element found",
+		},
+	}
+
+	for i, test := range tests {
+		stream := Stream{
+			Limit:  test.limit,
+			Length: 0,
+		}
+
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+
+			for _, element := range test.elements {
+				stream.Append(&element)
+			}
+
+			_, err := stream.Get(test.key)
+
+			if err != nil && err.Error() != test.errorExpected {
+				t.Errorf("Test failed: wrong error: %q\ngot:  %q", err.Error(), test.errorExpected)
+			}
+		})
+	}
 }
